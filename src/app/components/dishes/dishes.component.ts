618b535e26fb970601dd664d7172b9e29784980e
ppiotrekp <ppyrczak5@gmail.com>
2022-12-27 00:41:19 +0100

    dishes' cards without image done

--- src/app/components/dishes/dishes.component.ts
+++ src/app/components/dishes/dishes.component.ts
@@ -8,13 +8,31 @@ import dishes from './dishes.json'
   styleUrls: ['./dishes.component.css']
 })
 export class DishesComponent implements OnInit {
-  dishes:{image:String, name:String, typeOfCuisine:String,
-    typeOfMeal:String, ingredients:Array<String>,
-    limit:number, price:number, description:String
+  dishes:{id:number, image:string, name:string, typeOfCuisine:string,
+    typeOfMeal:string, ingredients:Array<String>,
+    limit:number, price:number, description:string
   }[] = dishes;
+  amount: number = 0;
   constructor() { }
 
+  increaseAmountOfDishes(id: number) {
+    var selector = document.getElementById(id.toString())
+    this.amount ++;
+    // @ts-ignore
+    selector.innerText = this.amount.toString();
+  }
+
+  decreaseAmountOfDishes(id: number) {
+    var selector = document.getElementById(id.toString())
+    this.amount --;
+    // @ts-ignore
+    selector.innerText = this.amount.toString();
+  }
+
   ngOnInit(): void {
+
+    // const plusButton = document.getElementsByClassName("fas fa-plus-square");
+    // plusButton[0].addEventListener('click', updateAmountOfDishes);
   }
 
 
