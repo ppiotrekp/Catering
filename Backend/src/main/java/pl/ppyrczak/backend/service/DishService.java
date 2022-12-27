0f0fdcaaf34b8ef9afc1bc79863eb6da4c77ea7e
ppyrc <ppyrczak@student.agh.edu.pl>
2022-12-27 16:51:57 +0100

    dish service created

--- Backend/src/main/java/pl/ppyrczak/backend/service/DishService.java
+++ Backend/src/main/java/pl/ppyrczak/backend/service/DishService.java
@@ -1,2 +1,7 @@
-package pl.ppyrczak.backend.service;public class DishService {
+package pl.ppyrczak.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishService {
 }
