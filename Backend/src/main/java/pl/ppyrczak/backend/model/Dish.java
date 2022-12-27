8873e4f9967e07ce8bf4c726a1accef79480b88a
ppyrc <ppyrczak@student.agh.edu.pl>
2022-12-27 16:42:32 +0100

    dish entity created

--- Backend/src/main/java/pl/ppyrczak/backend/model/Dish.java
+++ Backend/src/main/java/pl/ppyrczak/backend/model/Dish.java
@@ -1,2 +1,25 @@
-package pl.ppyrczak.backend.model;public class Dish {
+package pl.ppyrczak.backend.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import pl.ppyrczak.backend.util.Cuisine;
+import pl.ppyrczak.backend.util.Meal;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Document
+public class Dish {
+  @Id
+  private String id;
+  private String imageUrl;
+  private String name;
+  private Cuisine cuisine;
+  private Meal meal;
+  private List<String> ingredients;
+  private int limit;
+  private BigDecimal price;
+  private String description;
 }
