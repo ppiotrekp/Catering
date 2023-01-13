93c341b1a250f3c1e7c0d88f3177f166db6f43fd
ppyrc <ppyrczak@student.agh.edu.pl>
2023-01-13 18:27:51 +0100

    new header for home menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 
 @Component({
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showHeader = true;
 
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof  NavigationEnd) {
+        if (val.url =='/home') {
+          this.showHeader = false;
+        }
+      }
+    })
+  }
 }
